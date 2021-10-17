23db2a94be40301d29fb0bb3e26a632b7b8356ce
phutthaphongs01 <phutthaphongs61@nu.ac.th>
2021-10-17 22:37:13 +0700

    โปรแกรมหาส่วนลด

---
+++ โปรแกรมหาจำนวนเงินส่วนลด .py
@@ -0,0 +1,6 @@
+price = float(input("Price: "))
+discount_percent = int(input("Discount(%): "))
+discount_bath = discount_percent / 100*price
+payment = price - discount_bath
+print("Discount(baht): %d"%discount_bath)
+print("Patment: %d"%payment) 
